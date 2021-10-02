50a6a376477aa7ce86280ecd6d8376d2b5329b22
3nzoo <florenzodinglasan@gmail.com>
2021-10-02 20:23:26 +0800

    added eslint and sample usetatate component, react-refresh-webpack plugin

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './styles.css'
 import IMAGE from './funny.png'
 import LOGO from './monkey-emojis-6.svg'
+import { SampleComponent } from './SampleComponent'
 
 export const App = () => {
     return <>
-    <h1> React Typescript Webpack Template - {process.env.NODE_ENV} {process.env.name}</h1>
+    <h1> React Typescript Webpack Template s - {process.env.NODE_ENV} {process.env.name}</h1>
     <img src={IMAGE} alt="funny pic" width='300' height='300' />
     <img src={LOGO} alt="monkey" width='200' height='200' />
+    <SampleComponent/>
     </>
 }
---
+++ src/SampleComponent.tsx
@@ -0,0 +1,10 @@
+import { useState } from "react"
+
+export const SampleComponent = () => {
+    const [count, setCount] = useState(0);
+    return (
+        <div>
+            <button onClick={() => setCount(counter => counter + 1)}> Count {count}</button>
+        </div>
+    )
+}
