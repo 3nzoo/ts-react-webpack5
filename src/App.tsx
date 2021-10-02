eb4ef9ce81a2cae18966205981267dc523d28291
3nzoo <florenzodinglasan@gmail.com>
2021-10-02 19:41:49 +0800

    added dec.d.ts, png,svg rules and production webpack config

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,11 @@
+import './styles.css'
+import IMAGE from './funny.png'
+import LOGO from './monkey-emojis-6.svg'
+
 export const App = () => {
-    return <h1> React Typescript Webpack Template</h1>
+    return <>
+    <h1> React Typescript Webpack Template - {process.env.NODE_ENV} {process.env.name}</h1>
+    <img src={IMAGE} alt="funny pic" width='300' height='300' />
+    <img src={LOGO} alt="monkey" width='200' height='200' />
+    </>
 }
